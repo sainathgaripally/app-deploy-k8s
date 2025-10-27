3482d06834027172722a1ff8a99830bbcfce78e3
sainathgaripally <97355929+sainathgaripally@users.noreply.github.com>
2025-10-27 08:03:59 +0530

    Create CalculatorTest.java

---
+++ src/test/java/com/example/app/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+}
